6e8348877b2aab47037d90e473ceca014080673c
Aleksnadr Zhylinskiy <aleksander.zhylinskiy@miritec.com>
2016-03-17 18:43:35 +0200

    added angularjs, jquery, parts table migration and part controller

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,10 +13,23 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
+    <script type="text/javascript" src="{{ URL::asset('public/js/angular.min.js')  }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('public/js/jquery-1.12.1.min.js') }}"></script>
+
+    <script type="text/javascript" src="{{ URL::asset('public/js/script.js') }}"></script>
+
 </head>
 <body>
     <div class="container">
         @yield('panel')
     </div>
     @yield('content')
+
+    <div ng-app="">
+
+        <p>Input something in the input box:</p>
+        <p>Name: <input type="text" ng-model="name"></p>
+        <p ng-bind="name"></p>
+
+    </div>
 </body>
